caf820d18b99168e5c985a40bbe429cd01d45f12
Damir <damir24may@mail.ru>
2023-02-05 18:58:41 +0300

    hard03

--- script.js
+++ script.js
@@ -1,89 +1,15 @@
-"use strict";
-
-let lang = "ru";
-
-if (lang == "ru") {
-  console.log(
-    "Понедельник",
-    "Вторник",
-    "Среда",
-    "Четверг",
-    "Пятница",
-    "Суббота",
-    "Воскресенье"
-  );
-} else if (lang == "en") {
-  console.log(
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday"
-  );
-} else {
-  console.log("Другое значение");
-}
-
-lang = "en";
-
-switch (lang) {
-  case "en":
-    console.log(
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-      "Sunday"
-    );
-    break;
-  case "ru":
-    console.log(
-      "Понедельник",
-      "Вторник",
-      "Среда",
-      "Четверг",
-      "Пятница",
-      "Суббота",
-      "Воскресенье"
-    );
-    break;
-  default:
-    console.log("Другое значение");
-}
-
-lang = "ru";
-
-week = {
-  ru: [
-    "Понедельник",
-    "Вторник",
-    "Среда",
-    "Четверг",
-    "Пятница",
-    "Суббота",
-    "Воскресенье",
-  ],
-  en: [
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday",
-  ],
+let stringConverter = function (arg) {
+  let result = "Объявлена не строка";
+  if (typeof arg === "string") {
+    let maxLength = 30;
+    let string = arg.trim();
+    if (string.length > maxLength) {
+      result = string.substr(0, maxLength) + "...";
+    } else {
+      result = string;
+    }
+  }
+  return result;
 };
 
-console.log(week[lang]);
-
-let namePerson = "Александр";
-
-namePerson == "Артем"
-  ? console.log("директор")
-  : namePerson == "Александр"
-  ? console.log("преподаватель")
-  : console.log("студент");
+console.log(stringConverter());
