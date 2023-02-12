41412d8b04af73b75adfe83d30b1f1503262730b
Damir <damir24may@mail.ru>
2023-02-13 01:52:18 +0300

    hard05

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
-let stringConverter = function (arg) {
-  let result = "Объявлена не строка";
-  if (typeof arg === "string") {
-    let maxLength = 30;
-    let string = arg.trim();
-    if (string.length > maxLength) {
-      result = string.substr(0, maxLength) + "...";
-    } else {
-      result = string;
+"use strict";
+
+let arr = ["12345", "54321", "67891", "92392", "2435435", "4671203", "41756"];
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i].startsWith("2") || arr[i].startsWith("4")) {
+    console.log(arr[i]);
+  }
+}
+
+const dividers = function (num) {
+  let arr = [];
+  if (num !== 1) {
+    arr.push(1);
+    for (let i = 2; i * i <= num; i++) {
+      if (num % i === 0) {
+        arr.push(i);
+      }
     }
   }
-  return result;
+  arr.push(num);
+  return arr;
 };
 
-console.log(stringConverter());
+for (let i = 1; i <= 100; i++) {
+  const n = dividers(i);
+  if (n.length <= 2) {
+    console.log(i + ": Простое число. Делители  числа: " + n.join(", "));
+  }
+}
+
+for (let i = 1; i <= 100; i++) {
+  console.log(i + ": Делители  числа: " + dividers(i).join(", "));
+}
+
+dividers();
